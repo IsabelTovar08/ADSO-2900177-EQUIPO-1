a1b9093a64fc0e829e83b740d7460316590e8182
nataliosorio <159502099+nataliosorio@users.noreply.github.com>
2024-04-25 16:28:40 -0500

    Add files via upload

---
+++ Conversiones-Natalia/js/conversiones.js
@@ -0,0 +1,47 @@
+//Conversiones de longitud 
+let turquia =6000000
+let italia =5000000
+
+function salario(pdiasTrab, pvalorDia){
+    let diasTrab = pdiasTrab;
+    let valorDia = pvalorDia
+    let sueldo = diasTrab * valorDia;
+    return sueldo
+}
+
+function ahorros(psueldo){
+    let sueldo = psueldo;
+    let ahorro = sueldo * 6;
+    return ahorro
+}
+
+function viaje(pahorroTotal){
+    let ahorroTotal = pahorroTotal
+    if (ahorroTotal>= turquia){
+        let sobrante =ahorroTotal-turquia;
+        if (sobrante>0){
+            let compras;
+        compras = sobrante*0.4;
+        let donar = sobrante * 0.6
+        return "Dinero para compras: "+compras+"\n"+
+               "Dinero para donar: "+donar;
+        }else{
+            return "No le sobrara dinero"
+        } 
+     }
+    else{
+        if(ahorroTotal<turquia && ahorroTotal>=italia){
+            
+            return "Podra viajar a Italia"
+
+        }else{
+            return "No podra viajar"
+        }
+        
+    }}
+
+
+
+
+
+
